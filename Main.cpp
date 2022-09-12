dd04244a1882afd56ad94b5e93bfa1c7836cf34a
Abdullah <mabdullahafzal09@gmail.com>
2022-09-13 02:02:29 +0500

    Correction of Choice Control

--- Main.cpp
+++ Main.cpp
@@ -5,17 +5,19 @@
 using namespace std;
 
 int main(){
-    int choice = 0 ; 
+    int choice = 1 ; 
     HomePage hmp;
     Grid g;
     hmp.print();
     hmp.Rules();
     do{
-        if(choice < 0 or choice > 3) printf("\x1B[31mEnter the Valid Choice \033[0m\n");
+        if(choice < 1 or choice > 3) printf("\x1B[31mEnter the Valid Choice \033[0m\n");
         hmp.playMenu();
         cout<<"Enter Choice : ";
         cin>>choice;
-    }while(choice < 0 or choice > 3 );
+        cin.clear();
+        cin.sync();
+    }while(choice < 1 or choice > 3 );
 
     if(choice == 2){
         g.create_R_Penotomio();
